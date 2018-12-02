0d61cbbe54de7b267f4158125014dde75953b4cc
luqman <luqman.fanani@gmail.com>
2018-12-02 17:36:20 +0800

    bugs create icon app

---
+++ public/js/sw.js
@@ -0,0 +1,7 @@
+// Siapkan fungsi Callback untuk event "install"
+self.addEventListener('install', function(event) {
+    // Lakukan beberapa pekerjaan
+    });
+    self.addEventListener('activate', function(event) {
+    // Lakukan aktifasi
+    });
